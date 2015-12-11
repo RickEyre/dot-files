bce82f780d8d5b91d27b96cbf0e749b9a6c84dd2
Rick Eyre <rick.eyre@outlook.com>
2015-12-11 14:51:20 -0500

    Add git configuration files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,4 @@ export DOT_FILES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -fs $DOT_FILES_PATH/zsh/zshrc.zsh ~/.zshrc
 ln -fs $DOT_FILES_PATH/npm/.npmrc ~/.npmrc
+ln -fs $DOT_FILES_PATH/git/.gitconfig ~/.gitconfig
