29a16c19bec22d4445fd7b4081c0e4708c57a5f6
Rick Eyre <rick.eyre@outlook.com>
2015-12-11 14:53:40 -0500

    Remove export of DOT_FILES_PATH.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DOT_FILES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOT_FILES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -fs $DOT_FILES_PATH/zsh/zshrc.zsh ~/.zshrc
 ln -fs $DOT_FILES_PATH/npm/.npmrc ~/.npmrc
